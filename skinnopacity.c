d58b48418a7ce80d2186c720f20a06d46a78a2ec
louis <louis.braun@gmx.de>
2012-12-05 21:09:10 +0100

    Version 0.0.4

--- skinnopacity.c
+++ skinnopacity.c
@@ -15,7 +15,7 @@
 #endif
 
 
-static const char *VERSION        = "0.0.3";
+static const char *VERSION        = "0.0.4";
 static const char *DESCRIPTION    = "'nOpacity' Skin";
 static const char *MAINMENUENTRY  = "nOpacity";
 
