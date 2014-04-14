cca206e7184e4ba9e2b07526eec6f402d92df804
louis <louis.braun@gmx.de>
2014-04-14 18:05:44 +0200

    Only display poster in detailed EPG View if really available

--- menudetailview.c
+++ menudetailview.c
@@ -618,7 +618,9 @@ void cNopacityMenuDetailEventView::SetContent(void) {
         contentWidth = width;
         contentX = 0;
         bool displayPoster = false;
-        if (isSeries || isMovie) {
+        if (isSeries && series.posters.size() > 0) {
+            displayPoster = true;
+        } else if (isMovie && (movie.poster.width > 0) && (movie.poster.height > 0) && (movie.poster.path.size() > 0)) {
             displayPoster = true;
         } else if (hasAdditionalMedia) {
             if (mediaInfo.posters.size() >= 1) {
