f45b53c9c22d13739d2b8885fec11a42bb03fa1b
kamel5 <vdr.kamel5 (at) gmx (dot) net>
2022-10-14 11:13:34 +0200

    Added NULL checking of pixmaps in volumebox.c
    If a pixmap cannot be created with cOsd::CreatedPixmap(), the return
    value is NULL. Because of this, a NULL check has been added to all
    pixmap functions to prevent a possible segfault.

--- volumebox.c
+++ volumebox.c
@@ -18,37 +18,39 @@ cNopacityVolumeBox::cNopacityVolumeBox(cOsd *osd, const cRect &Rect, cFont *Font
     if (simple) {
         if (altcolor) {
             tColor altBackgroundColor = Theme.Color(clrChannelBackground);
-            pixmapBackgroundVolume = osd->CreatePixmap(6, cRect(Rect.Left() - Rect.Height() / 4, Rect.Top(), Rect.Width() + Rect.Height() / 2, Rect.Height()));
-            pixmapBackgroundVolume->Fill(clrTransparent);
-            pixmapBackgroundVolume->DrawEllipse(cRect(0, 0, Rect.Height() / 2, Rect.Height()), altBackgroundColor, 7);
-            pixmapBackgroundVolume->DrawEllipse(cRect(Rect.Width(), 0, Rect.Height() / 2, Rect.Height()), altBackgroundColor, 5);
-            pixmapBackgroundVolume->DrawRectangle(cRect(Rect.Height() / 2, 0, Rect.Width() - Rect.Height() / 2, Rect.Height()), altBackgroundColor);
+            pixmapBackgroundVolume = CreatePixmap(osd, "pixmapBackgroundVolume", 6, cRect(Rect.Left() - Rect.Height() / 4, Rect.Top(), Rect.Width() + Rect.Height() / 2, Rect.Height()));
+            PixmapFill(pixmapBackgroundVolume, clrTransparent);
+            if (pixmapBackgroundVolume) {
+                pixmapBackgroundVolume->DrawEllipse(cRect(0, 0, Rect.Height() / 2, Rect.Height()), altBackgroundColor, 7);
+                pixmapBackgroundVolume->DrawEllipse(cRect(Rect.Width(), 0, Rect.Height() / 2, Rect.Height()), altBackgroundColor, 5);
+                pixmapBackgroundVolume->DrawRectangle(cRect(Rect.Height() / 2, 0, Rect.Width() - Rect.Height() / 2, Rect.Height()), altBackgroundColor);
+            }
         } else {
-            pixmapBackgroundVolume = osd->CreatePixmap(6, cRect(Rect));
-            pixmapBackgroundVolume->Fill(clrTransparent);
+            pixmapBackgroundVolume = CreatePixmap(osd, "pixmapBackgroundVolume", 6, cRect(Rect));
+            PixmapFill(pixmapBackgroundVolume, clrTransparent);
         }
 	int textHeight = font->Height();
         int textWidth = font->Width(*testlabel);
-        pixmapLabelVolume = osd->CreatePixmap(7, cRect(Rect.Left(), Rect.Top() + (Rect.Height() - textHeight) / 2, textWidth, textHeight));
-        pixmapLabelVolume->Fill(clrTransparent);
+        pixmapLabelVolume = CreatePixmap(osd, "pixmapLabelVolume", 7, cRect(Rect.Left(), Rect.Top() + (Rect.Height() - textHeight) / 2, textWidth, textHeight));
+        PixmapFill(pixmapLabelVolume, clrTransparent);
 
         barWidth = Rect.Width() - textWidth - 10;
         barHeight = Rect.Height() * 2 / 3;
         if (barHeight % 2 != 0)
             barHeight++;
         int barTop = (Rect.Height() - barHeight) / 2 + 1;
-        pixmapProgressBarVolume = osd->CreatePixmap(7, cRect(Rect.Left() + textWidth + 10, Rect.Top() + barTop, barWidth, barHeight));
-        pixmapProgressBarVolume->Fill(clrTransparent);
+        pixmapProgressBarVolume = CreatePixmap(osd, "pixmapProgressBarVolume", 7, cRect(Rect.Left() + textWidth + 10, Rect.Top() + barTop, barWidth, barHeight));
+        PixmapFill(pixmapProgressBarVolume, clrTransparent);
     } else {
-        pixmapBackgroundVolume = osd->CreatePixmap(6, cRect(Rect));
-        pixmapBackgroundVolume->Fill(clrTransparent);
+        pixmapBackgroundVolume = CreatePixmap(osd, "pixmapBackgroundVolume", 6, cRect(Rect));
+        PixmapFill(pixmapBackgroundVolume, clrTransparent);
         if (config.GetValue("displayType") == dtGraphical) {
             cImage *imgBack = imgCache->GetSkinElement(seVolumeBackground);
-            if (imgBack) {
+            if (pixmapBackgroundVolume && imgBack) {
                 pixmapBackgroundVolume->DrawImage(cPoint(0, 0), *imgBack);
             }
         } else {
-            pixmapBackgroundVolume->Fill(Theme.Color(clrChannelBackground));
+            PixmapFill(pixmapBackgroundVolume, Theme.Color(clrChannelBackground));
             if (config.GetValue("displayType") == dtBlending) {
                 DrawBlendedBackground(pixmapBackgroundVolume,
                                       0,
@@ -79,10 +81,10 @@ cNopacityVolumeBox::cNopacityVolumeBox(cOsd *osd, const cRect &Rect, cFont *Font
         barWidth = geoManager->volumeProgressBarWidth;
         barHeight = geoManager->volumeProgressBarHeight;
 
-        pixmapLabelVolume = osd->CreatePixmap(7, cRect(Rect.Left(), Rect.Top() + 5, geoManager->volumeWidth, geoManager->volumeLabelHeight));
-        pixmapLabelVolume->Fill(clrTransparent);
-        pixmapProgressBarVolume = osd->CreatePixmap(7, cRect((Rect.Left() + (geoManager->volumeWidth - barWidth) / 2), Rect.Top() + ((geoManager->volumeHeight - barHeight) * 2 / 3), barWidth, barHeight));
-        pixmapProgressBarVolume->Fill(clrTransparent);
+        pixmapLabelVolume = CreatePixmap(osd, "pixmapLabelVolume", 7, cRect(Rect.Left(), Rect.Top() + 5, geoManager->volumeWidth, geoManager->volumeLabelHeight));
+        PixmapFill(pixmapLabelVolume, clrTransparent);
+        pixmapProgressBarVolume = CreatePixmap(osd, "pixmapProgressBarVolume", 7, cRect((Rect.Left() + (geoManager->volumeWidth - barWidth) / 2), Rect.Top() + ((geoManager->volumeHeight - barHeight) * 2 / 3), barWidth, barHeight));
+        PixmapFill(pixmapProgressBarVolume, clrTransparent);
     }
 }
 
@@ -93,19 +95,23 @@ cNopacityVolumeBox::~cNopacityVolumeBox(void) {
 }
 
 void cNopacityVolumeBox::SetVolume(int Current, int Total, bool Mute) {
-    pixmapProgressBarVolume->Fill(clrTransparent);
-    pixmapLabelVolume->Fill(clrTransparent);
+    PixmapFill(pixmapProgressBarVolume, clrTransparent);
+    PixmapFill(pixmapLabelVolume, clrTransparent);
 
     int barHeight = geoManager->volumeProgressBarHeight;
     if ((Current == 0) && (Total == 0) || (barHeight < 5)) {
         return;
     }
 
-    pixmapProgressBarVolume->DrawEllipse(cRect(0, 0, barHeight, barHeight), Theme.Color(clrProgressBarBack));
-    pixmapProgressBarVolume->DrawEllipse(cRect(barWidth - barHeight, 0, barHeight, barHeight), Theme.Color(clrProgressBarBack));
-    pixmapProgressBarVolume->DrawRectangle(cRect(barHeight / 2, 0, barWidth - barHeight, barHeight), Theme.Color(clrProgressBarBack));
+    if (pixmapProgressBarVolume) {
+        pixmapProgressBarVolume->DrawEllipse(cRect(0, 0, barHeight, barHeight), Theme.Color(clrProgressBarBack));
+        pixmapProgressBarVolume->DrawEllipse(cRect(barWidth - barHeight, 0, barHeight, barHeight), Theme.Color(clrProgressBarBack));
+        pixmapProgressBarVolume->DrawRectangle(cRect(barHeight / 2, 0, barWidth - barHeight, barHeight), Theme.Color(clrProgressBarBack));
 
-    DrawProgressbar(pixmapProgressBarVolume, 1, 1, barWidth - 2, barHeight - 2, Current, Total, Theme.Color(clrProgressBar), Theme.Color(clrProgressBarBlend), true);
+        DrawProgressbar(pixmapProgressBarVolume, 1, 1, barWidth - 2, barHeight - 2, Current, Total, Theme.Color(clrProgressBar), Theme.Color(clrProgressBarBlend), true);
+    }
+    if (!pixmapLabelVolume)
+        return;
 
     cString label = cString::sprintf("%s: %d", tr("Volume"), Current);
     pixmapLabelVolume->DrawText(cPoint(textLeft, textTop), *label, Theme.Color(clrChannelHead), clrTransparent, font);
@@ -117,7 +123,7 @@ void cNopacityVolumeBox::SetVolume(int Current, int Total, bool Mute) {
 }
 
 void cNopacityVolumeBox::SetAlpha(int Alpha) {
-    pixmapBackgroundVolume->SetAlpha(Alpha);
-    pixmapProgressBarVolume->SetAlpha(Alpha);
-    pixmapLabelVolume->SetAlpha(Alpha);
+    PixmapSetAlpha(pixmapBackgroundVolume, Alpha);
+    PixmapSetAlpha(pixmapProgressBarVolume, Alpha);
+    PixmapSetAlpha(pixmapLabelVolume, Alpha);
 }
