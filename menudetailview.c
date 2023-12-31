7fb365a428d58cd7096b098b17e676ac0034d4be
Stefan Hofmann <stefan.hofmann@t-online.de>
2023-12-31 13:08:13 +0100

    Add frame parameters (VDR >= 2.6.5) to menudetailview

--- menudetailview.c
+++ menudetailview.c
@@ -380,6 +380,11 @@ std::string cNopacityDetailView::LoadRecordingInformation(void) {
     delete index;
 
     if (Info) {
+#if (APIVERSNUM >= 20605)
+        if (*Info->FrameParams()) {
+            sstrInfo << tr("Frame Parameters") << ": " << *Info->FrameParams() << std::endl;
+        }
+#endif
 #if (APIVERSNUM >= 20505)
         if (Info->Errors() >= 0) {
             cString errors = cString::sprintf("%s: %i ", tr("TS Errors"), Info->Errors());
