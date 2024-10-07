1974bfe3292e686ca7461a5b3266f9856651614c
kamel5 <vdr.kamel5 (at) gmx (dot) net>
2024-10-07 12:37:44 +0200

    Version 1.1.19

--- skinnopacity.c
+++ skinnopacity.c
@@ -16,7 +16,7 @@
 #error "VDR-1.7.34 API version or greater is required!"
 #endif
 
-static const char *VERSION        = "1.1.18";
+static const char *VERSION        = "1.1.19-Beta";
 static const char *DESCRIPTION    = "'nOpacity' Skin";
 static const char *MAINMENUENTRY  = "nOpacity";
 
